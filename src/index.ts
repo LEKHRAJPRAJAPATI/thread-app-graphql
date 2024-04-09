39d37d441fe4d7212174f6f500d7885c5fec14d2
Lekhraj prajapati <plekhraj838@gmail.com>
2024-04-09 16:35:28 +0530

    Add existing file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 
 // Create a apolo server
  async function init() {
@@ -13,11 +14,29 @@ const PORT  = Number(process.env.PORT) || 8000
         type Query {
             hello: String
             say(name: String): String
-        }`,
+        }
+        type Mutation {
+            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+        }
+        `,
         resolvers: {
             Query: {
                 hello: () => `Hey there, I am a graphql server`,
                 say: (_, {name}: {name: string}) => `Hey ${name}, How are you?`
+            },
+            Mutation: {
+                createUser: async(_, {firstName, lastName, email,password}: {firstName: string; lastName: string; email: string; password: string;}) => {
+                  await prismaClient.user.create({
+                     data: {
+                        email,
+                        firstName,
+                        lastName,
+                        password,
+                        salt: "random_salt",
+                     },   
+                  });
+                  return true;
+                },
             },
         },
      })
